49f1aba6b8084c55c3e7d8954dcd18814208703d
Red Thor <51344077+thoroftroy@users.noreply.github.com>
2022-09-21 12:27:16 -0400

    A bunch of kind of viruses
    I made some things to annoy people, they work okay.... Kinda outdated though

---
+++ xp.bat
@@ -0,0 +1,8 @@
+@ echo off
+rem ---------------------------------
+rem Start Windows XP Secret Song
+start "" "%systemroot%\system32\oobe\images\title.wma"
+rem ---------------------------------
+
+
+__-Virus Author: red-__
